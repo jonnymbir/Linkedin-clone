5735c5f380c5356c63ef869eb307e680ebedde3e
John Kofi Mbir <jonnymbir@gmail.com>
2022-01-30 23:05:01 +0000

    next auth completed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../lib/mongodb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +12,13 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-})
+  secret: process.env.JWT_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  pages: {
+    signIn: "/home",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  debug: true,
+});
