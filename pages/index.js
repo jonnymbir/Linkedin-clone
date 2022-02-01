397431beda27dbfc0759893342666fe0340fa2ba
John Kofi Mbir <jonnymbir@gmail.com>
2022-02-01 04:39:57 +0000

    Feed and modal animateion added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
+import { AnimatePresence } from "framer-motion";
 import { getSession, useSession } from "next-auth/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
+import Modal from "../components/Modal";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
+
   const router = useRouter();
   const { status } = useSession({
     required: true,
@@ -26,7 +34,15 @@ export default function Home() {
       <main className="flex justify-center gap-x-5 px-4 sm:px-12">
         <div className="flex flex-col md:flex-row gap-5">
           <Sidebar/>
+          <Feed/>
         </div>
+
+        <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence>
+
       </main>
     </div>
   );
