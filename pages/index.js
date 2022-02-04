812a0aad782d8d7bd1c1e0d411af2cbf1048d107
John Kofi Mbir <jonnymbir@gmail.com>
2022-02-04 22:34:19 +0000

    Authenticateion and Router integrated

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import { getSession, useSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated(){
+      router.push("/home");
+    }
+  });
+  
   return (
     <div className="bg-[#F3F2EF] dark:bg-black dar:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
@@ -20,4 +30,22 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context);
+  if(!session){
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/home"
+      }
+    }
+  }
+
+  return {
+    props:{
+      session,
+    }
+  }
 }
