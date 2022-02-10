209a691ad3e6badfe75d2fe9d8ebeced928e8c93
John Kofi Mbir <jonnymbir@gmail.com>
2022-02-10 19:31:25 +0000

    Sign out completed

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,4 +1,7 @@
+import { signOut, useSession } from "next-auth/react";
+
 function HeaderLink({ Icon, text, avatar, feed, active, hidden }) {
+  const { data: session } = useSession();
   return (
     <div
       className={`${hidden && "hidden md:inline-flex"} cursor-pointer flex flex-col justify-items-center items-center ${
@@ -6,8 +9,9 @@ function HeaderLink({ Icon, text, avatar, feed, active, hidden }) {
           ? "text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1"
           : "text-gray-500 hover:text-gray-700"
       } ${active && "!text-black dark:!text-white"}`}
+      onClick={() => avatar && signOut()}
     >
-      {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
+      {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" src={session?.user?.image} /> : <Icon />}
 
       <h4 className={`text-sm ${feed && "hidden lg:flex justify-center w-full mx-auto"}`}>{text}</h4>
 
